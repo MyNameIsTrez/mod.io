03709f93c81afa1bede30bf41542e92e21f4b934
Clement <clement.julia13@gmail.com>
2018-08-22 16:29:23 +0200

    final update of example to use async

--- examples/newfile.py
+++ examples/newfile.py
@@ -1,17 +1,27 @@
-import modio
+import async_modio as modio
+import asyncio
 
-client = modio.Client(auth="oauth2 token goes here")
-newfile = modio.NewModFile(
-        version="0.3.4",
-        changelog = "New version which now contains <b> twice as many toilets </b>",
-        active=True,
-        metadata = "new_file,test_file,crazy"
-    )
+async def example():
+    client = modio.Client(auth="oauth2 token goes here")
+    newfile = modio.NewModFile(
+            version="0.3.4",
+            changelog = "New version which now contains <b> twice as many toilets </b>",
+            active=True,
+            metadata = "new_file,test_file,crazy"
+        )
 
-newfile.add_file("path/to/file/new_release.zip")
+    newfile.add_file("path/to/file/new_release.zip")
 
-mod = client.get_game(234).get_mod(567)
-modfile = mod.add_file(newfile)
+    mod = await client.get_game(234).get_mod(567)
+    modfile = await mod.add_file(newfile)
 
-print(modfile)
-#<modio.ModFile name=new_release.zip version=0.3.4 mod=567>
+    print(modfile)
+    #<modio.ModFile name=new_release.zip version=0.3.4 mod=567>
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(example())
+    loop.close()
+
+if __name__ == '__main__':
+      main() 
--- examples/newmod.py
+++ examples/newmod.py
@@ -1,22 +1,32 @@
-import modio
+import async_modio as modio
+import asyncio
 
-client = modio.Client(auth="oauth2 token goes here")
-newmod = modio.NewMod(
-        name="A LOTR Toilet Mod",
-        name_id = "lotr-toilets",
-        summary="Embark on a fascinating journey through Middle Earth's most beautiful toilets.",
-        description = "<h1>It's a very good mod</h1><br><h2>You should play it</h2>",
-        homepage = "https://me.toilets.lotr",
-        stock = 400,
-        maturity = 11, #alcohol, drugs, explicit
-        metadata="toilets,lotr,hd,cool"
-    )
+async def example():
+    client = modio.Client(auth="oauth2 token goes here")
+    newmod = modio.NewMod(
+            name="A LOTR Toilet Mod",
+            name_id = "lotr-toilets",
+            summary="Embark on a fascinating journey through Middle Earth's most beautiful toilets.",
+            description = "<h1>It's a very good mod</h1><br><h2>You should play it</h2>",
+            homepage = "https://me.toilets.lotr",
+            stock = 400,
+            maturity = 11, #alcohol, drugs, explicit
+            metadata="toilets,lotr,hd,cool"
+        )
 
-newmod.add_tags("texture overhaul", "quality", "medium")
-newmod.add_logo("path/to/file/lotr_toilet.jpg")
+    newmod.add_tags(["texture overhaul", "quality", "medium"])
+    newmod.add_logo("path/to/file/lotr_toilet.jpg")
 
-game = client.get_game(234)
-mod = game.add_mod(newmod)
+    game = await client.get_game(234)
+    mod = await game.add_mod(newmod)
 
-print(mod)
-#<modio.Mod id=567 name=A LOTR Toilet Mod game=234>
+    print(mod)
+    #<modio.Mod id=567 name=A LOTR Toilet Mod game=234>
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(example())
+    loop.close()
+
+if __name__ == '__main__':
+      main() 
