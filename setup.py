68bf8bac55c7ec8f316907a6099826740db1aef4
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2020-12-25 12:55:21 +0100

    Removed the "async_modio" .egg because it most likely is preventing pip install from working

--- setup.py
+++ setup.py
@@ -11,15 +11,3 @@ setup(name='modio',
         'requests>=2.20.0'
       ]
      )
-
-setup(name='async_modio',
-      version='0.2.0',
-      description='async mod.io python wrapper',
-      author='Clement Julia',
-      author_email='clement.julia13@gmail.com',
-      url='https://github.com/ClementJ18/mod.io',
-      packages=['async_modio'],
-      install_requires=[
-        'aiohttp==3.4.4'
-      ]
-     )
